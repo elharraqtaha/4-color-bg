ed1594e6ca21320a5aeb489cd79b50e29cacccce
BEN ALLAL <Zakariae>
2015-01-13 00:37:07 +0100

    multilang final
    Ajouter les autres titres textes
    
    id:text associé

--- js/langage.js
+++ js/langage.js
@@ -1,8 +1,20 @@
-
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
 var langage = [];
 var selectedLangage = "en";
+if(getParameterByName('lang')=='ar' && getParameterByName('lang') != null){
+		selectedLangage = 'ar';
+		
+}
 
+
+
+console.log(sessionStorage.getItem('lang'));
 
 $.ajax({
 		url : selectedLangage + ".json",
@@ -11,4 +23,6 @@ $.ajax({
 		},
 		async:false
  });
+ 
+ 
  
