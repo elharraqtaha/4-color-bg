01990bdcdf6f9933cc36851bb4c00d20af4113a1
BEN ALLAL <Zakariae>
2015-01-17 23:54:39 +0100

    A big change
    Ajoute de button de replay

--- js/playvideo.js
+++ js/playvideo.js
@@ -3,11 +3,36 @@ var videoElement = $("#videobcg")[0];
 
 videoElement.play();
 
-videoElement.onended = function(e) {
-    alert("isfinished");
-    var elem="#page-top .pattern";
 
-       $('html,body').animate({
+var $msg = $('#restartvideo'); 
+$msg.css({
+    top: (videoElement.offsetTop + ((videoElement.offsetHeight/2) - ($msg.height()/2))),
+    left:(videoElement.offsetLeft + ((videoElement.offsetWidth/2) - ($msg.width()/2)))
+});
+
+
+
+
+
+
+videoElement.onended = function(e) {
+    
+    $msg.css({
+    top: (videoElement.offsetTop + ((videoElement.offsetHeight/2) - ($msg.height()/2))),
+    left:(videoElement.offsetLeft + ((videoElement.offsetWidth/2) - ($msg.width()/2)))
+    }).addClass("show");
+      
+    
+    var elem="#page-top .pattern";
+     $('html,body').animate({
         scrollTop: $(elem).offset().top},
-        'slow');
+      'slow');
+    
+    
  };
+
+ $msg.click(function(){
+    videoElement.play();
+     $msg.removeClass("show");
+ });
+ 
